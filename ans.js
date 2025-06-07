726067b3d1fb9eec0fbc924d1ba6be542a0e51d0
Mirumo <mysteryperson520@gmail.com>
2025-06-08 03:53:45 +0800

    add ans

--- ans.js
+++ ans.js
@@ -4,7 +4,7 @@ const hints1 = [
   { label: 'Mission 1', content: 'C：「我不是好人」' },
   { label: 'Mission 1', content: '好人只會說實話，壞人只會說謊' },
   { label: 'Mission 1', content: 'A：「我不是壞人」' },
-]
+] // A
 
 const hints2 = [
   { label: 'Mission 2', content: 'C：「我和 D 都是清白的。」' },
@@ -14,14 +14,15 @@ const hints2 = [
   { label: 'Mission 2', content: '誰洩漏了機密？' },
   { label: 'Mission 2', content:  'A：「不是我，也不是 C。」' } ,
   { label: 'Mission 2', content: 'B：「A 沒有洩漏資料。」' },
-]
+] // C 說謊, D 洩漏
 
 const hints3 = [
-  { label: 'Mission 3', content: 'B：「我是第三個」' },
+  { label: 'Mission 3', content: 'B：「C 不是最後」' },
   { label: 'Mission 3', content: 'C：「A 不是最後」' },
   { label: 'Mission 3', content: '有三個人排隊' },
   { label: 'Mission 3', content: 'A：「B 在我前面」' },
-]
+  { label: 'Mission 3', content: '三個人排序是什麼？' },
+] // C B A
 
 const hints4 = [
   { label: 'Mission 4', content: '某研討會上，有誠實國跟說謊國的學者們參加' },
@@ -30,16 +31,16 @@ const hints4 = [
   { label: 'Mission 4', content: '但坐在主席左邊的人卻在會議記錄簿寫下總共有 8 個人參加' },
   { label: 'Mission 4', content: '每個人都說坐在自己右邊的是說謊國的人' },
   { label: 'Mission 4', content: '這個會議到底有多少人參加？' },
-]
+] // 8
 
 const hints5 = [
   { label: 'Mission 5', content: 'A、B、C、D、E、F六個人圍著圓桌坐下' },
-  { label: 'Mission 5', content: 'B是坐在A右邊的第二人' },
-  { label: 'Mission 5', content: 'E和F兩人不相鄰' },
-  { label: 'Mission 5', content: 'E坐在D的正對面' },
-  { label: 'Mission 5', content: 'F是坐C在左邊的第二人' },
+  { label: 'Mission 5', content: 'B 是坐在 A 右邊的第二人' },
+  { label: 'Mission 5', content: 'E 和 F 兩人不相鄰' },
+  { label: 'Mission 5', content: 'E 坐在 D 的正對面' },
+  { label: 'Mission 5', content: 'F 是坐 C 在左邊的第二人' },
   { label: 'Mission 5', content: '從A開始順時鐘算起，他們六個人是怎麼坐的呢？' },
-]
+] // A C D F B E
 
 const hints6 = [
   { label: 'Mission 6', content: '媽媽剛烤好放在桌上的餅乾被吃光了' },
@@ -51,5 +52,5 @@ const hints6 = [
   { label: 'Mission 6', content: '只有一個人吃了餅乾' },
   { label: 'Mission 6', content: '且五兄弟裡只有三個人說了實話' },
   { label: 'Mission 6', content: '誰吃了餅乾？' },
-]
+] // 5
 
