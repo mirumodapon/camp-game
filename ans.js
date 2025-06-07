b1863404e35e837d33c0d7bfc557434ac6ab5d53
Mirumo <mysteryperson520@gmail.com>
2025-06-08 03:13:16 +0800

    init

---
+++ ans.js
@@ -0,0 +1,55 @@
+const hints1 = [
+  { label: 'Mission 1', content: '誰是壞人？' },
+  { label: 'Mission 1', content: 'B：「我不是平民」' },
+  { label: 'Mission 1', content: 'C：「我不是好人」' },
+  { label: 'Mission 1', content: '好人只會說實話，壞人只會說謊' },
+  { label: 'Mission 1', content: 'A：「我不是壞人」' },
+]
+
+const hints2 = [
+  { label: 'Mission 2', content: 'C：「我和 D 都是清白的。」' },
+  { label: 'Mission 2', content: 'D：「不是 B 洩漏的。」' },
+  { label: 'Mission 2', content: 'E：「C 沒有說實話。」' },
+  { label: 'Mission 2', content: '有一個人說謊，還有一個人洩漏了機密' },
+  { label: 'Mission 2', content: '誰洩漏了機密？' },
+  { label: 'Mission 2', content:  'A：「不是我，也不是 C。」' } ,
+  { label: 'Mission 2', content: 'B：「A 沒有洩漏資料。」' },
+]
+
+const hints3 = [
+  { label: 'Mission 3', content: 'B：「我是第三個」' },
+  { label: 'Mission 3', content: 'C：「A 不是最後」' },
+  { label: 'Mission 3', content: '有三個人排隊' },
+  { label: 'Mission 3', content: 'A：「B 在我前面」' },
+]
+
+const hints4 = [
+  { label: 'Mission 4', content: '某研討會上，有誠實國跟說謊國的學者們參加' },
+  { label: 'Mission 4', content: '所有人圍坐在一圓桌旁' },
+  { label: 'Mission 4', content: '主席說：『會議開始吧！這次總共有 9 個人參加。』' },
+  { label: 'Mission 4', content: '但坐在主席左邊的人卻在會議記錄簿寫下總共有 8 個人參加' },
+  { label: 'Mission 4', content: '每個人都說坐在自己右邊的是說謊國的人' },
+  { label: 'Mission 4', content: '這個會議到底有多少人參加？' },
+]
+
+const hints5 = [
+  { label: 'Mission 5', content: 'A、B、C、D、E、F六個人圍著圓桌坐下' },
+  { label: 'Mission 5', content: 'B是坐在A右邊的第二人' },
+  { label: 'Mission 5', content: 'E和F兩人不相鄰' },
+  { label: 'Mission 5', content: 'E坐在D的正對面' },
+  { label: 'Mission 5', content: 'F是坐C在左邊的第二人' },
+  { label: 'Mission 5', content: '從A開始順時鐘算起，他們六個人是怎麼坐的呢？' },
+]
+
+const hints6 = [
+  { label: 'Mission 6', content: '媽媽剛烤好放在桌上的餅乾被吃光了' },
+  { label: 'Mission 6', content: '老大說：『是老四吃的。』' },
+  { label: 'Mission 6', content: '老二說：『我沒有吃。』' },
+  { label: 'Mission 6', content: '老三說：『不是老五吃的。』' },
+  { label: 'Mission 6', content: '老四說：『大哥說謊。』' },
+  { label: 'Mission 6', content: '老五說：『二哥說的是實話。』' },
+  { label: 'Mission 6', content: '只有一個人吃了餅乾' },
+  { label: 'Mission 6', content: '且五兄弟裡只有三個人說了實話' },
+  { label: 'Mission 6', content: '誰吃了餅乾？' },
+]
+
---
+++ circle/index.js
@@ -0,0 +1,119 @@
+const hints = [
+  { label: 'mission 1', content: 'tempor esse exercitation' },
+  { label: 'mission 1', content: 'sint consequat dolor' },
+  { label: 'mission 1', content: 'consequat laboris sint' },
+  { label: 'mission 1', content: 'anim ipsum aliquip' },
+]
+
+
+let main;
+let nodes;
+const center = {};
+const touch = { mousedown: false };
+
+
+function init() {
+  main = document.getElementById('app');
+
+  center.x = window.innerWidth / 2;
+  center.y = window.innerHeight / 2;
+
+
+  nodes = hints.map(hint => {
+    const node = document.createElement('span');
+    node.innerText = `[${hint.label}]: ${hint.content}`;
+
+    return node
+  });
+
+  main.style.clipPath = `circle(var(--circle-size) at ${center.x}px ${center.y}px)`;
+
+  nodes.forEach(node => {
+    node.classList.add('hint');
+    main.appendChild(node);
+  });
+
+  render();
+}
+
+function render() {
+
+  nodes.forEach((node) => {
+    const x = window.innerWidth - node.clientWidth;
+    const y = window.innerHeight - node.clientHeight;
+
+    const randomX = Math.floor(Math.random() * x);
+    const randomY = Math.floor(Math.random() * y);
+
+    node.style.left = `${randomX}px`;
+    node.style.top = `${randomY}px`;
+  })
+
+
+}
+
+function onResize() {
+  center.x = window.innerWidth / 2;
+  center.y = window.innerHeight / 2;
+}
+
+function onTouchStart(e) {
+  e.preventDefault();
+
+  if (e instanceof MouseEvent) {
+    touch.x = e.clientX;
+    touch.y = e.clientY;
+    touch.mousedown = true;
+  }
+
+  if (e instanceof TouchEvent) {
+    touch.x = e.touches[0].clientX;
+    touch.y = e.touches[0].clientY;
+  }
+}
+
+function onTouchMove(e) {
+  e.preventDefault();
+
+  let dx, dy;
+
+  if (e instanceof MouseEvent) {
+    dx = e.clientX - touch.x;
+    dy = e.clientY - touch.y;
+
+    if (!touch.mousedown) {
+      return;
+    }
+  }
+
+  if (e instanceof TouchEvent) {
+    dx = e.touches[0].clientX - touch.x;
+    dy = e.touches[0].clientY - touch.y;
+  }
+
+  main.style.clipPath = `circle(100px at ${center.x + dx}px ${center.y + dy}px)`;
+}
+
+function onTouchEnd(e) {
+  e.preventDefault();
+
+  if (e instanceof MouseEvent) {
+    touch.mousedown = false;
+  }
+
+  main.style.clipPath = `circle(100px at ${center.x}px ${center.y}px)`;
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('resize', onResize)
+
+document.addEventListener('touchstart', onTouchStart, { passive: false });
+document.addEventListener('touchmove', onTouchMove, { passive: false });
+document.addEventListener('touchend', onTouchEnd, { passive: false });
+
+document.addEventListener('mousedown', onTouchStart, { passive: false });
+document.addEventListener('mousemove', onTouchMove, { passive: false });
+document.addEventListener('mouseup', onTouchEnd, { passive: false });
+
+setInterval(render, 3000);
